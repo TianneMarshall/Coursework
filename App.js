cf7dd5f097ca41b91e60754551d9bc40253240f1
TianneMarshall <tianne.marshall@stu.mmu.ac.uk>
2021-02-08 15:26:26 +0000

    added stack navigation for home screen to App.js file

--- App.js
+++ App.js
@@ -3,14 +3,27 @@ import 'react-native-gesture-handler';
 import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import Homescreen from './components/homescreen';
 import ProfileScreen from './components/profilescreen';
 import SignUpScreen from './components/signupscreen';
 import LoginScreen from './components/loginscreen';
 import LogoutScreen from './components/logoutscreen';
+import LocationScreen from './components/locationscreen';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function HomeNav() {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Homescreen} />
+      <Stack.Screen name="Location" component={LocationScreen} />
+    </Stack.Navigator>
+  );
+}
+
 
 class App extends Component {
   constructor(props){
@@ -20,10 +33,11 @@ class App extends Component {
 
   render() {
     return (
+
       <NavigationContainer>
         <Tab.Navigator>
           <Tab.Screen name="Sign In" component={LoginScreen} />
-          <Tab.Screen name="Home" component={Homescreen} />
+          <Tab.Screen name="Home" component={HomeNav} />
           <Tab.Screen name="Register" component={SignUpScreen} />
           <Tab.Screen name="Profile" component={ProfileScreen} />
           <Tab.Screen name="Sign out" component={LogoutScreen} />
