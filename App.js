90fb8784286ab2b497a539857fc9e89655b7d4e2
TianneMarshall <tianne.marshall@stu.mmu.ac.uk>
2021-02-15 20:36:52 +0000

    Updated navigation to include sign in stack navigation

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
+import  Icon from 'react-native-vector-icons/FontAwesome';
 
 import Homescreen from './components/homescreen';
 import ProfileScreen from './components/profilescreen';
@@ -14,17 +15,20 @@ import LogoutScreen from './components/logoutscreen';
 import LocationScreen from './components/locationscreen';
 import EditProfileScreen from './components/editprofilescreen';
 import Reviews from './components/reviews';
+import WriteReviewScreen from './components/writeReviewScreen';
 import Practice from './components/practice';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 const UserStack = createStackNavigator();
+const LoginStack = createStackNavigator();
 
 function UserNav() {
   return(
     <UserStack.Navigator>
       <UserStack.Screen name="Profile" component={ProfileScreen} />
       <UserStack.Screen name="EditUser" component={EditProfileScreen} />
+      <UserStack.Screen name="LocationScreen" component={LocationScreen} />
     </UserStack.Navigator>
   );
 }
@@ -35,6 +39,17 @@ function HomeNav() {
       <Stack.Screen name="Home" component={Homescreen} />
       <Stack.Screen name="LocationScreen" component={LocationScreen} />
       <Stack.Screen name="Location" component={Location} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="WriteReviewScreen" component={WriteReviewScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function LoginNav() {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name="Sign In" component={LoginScreen} />
+      <Stack.Screen name="Register" component={SignUpScreen} />
     </Stack.Navigator>
   );
 }
@@ -51,12 +66,10 @@ class App extends Component {
 
       <NavigationContainer>
         <Tab.Navigator>
-          <Tab.Screen name="Sign In" component={LoginScreen} />
+          <Tab.Screen name="Sign In" component={LoginNav} />
           <Tab.Screen name="Home" component={HomeNav} />
-          <Tab.Screen name="Register" component={SignUpScreen} />
           <Tab.Screen name="Profile" component={UserNav} />
           <Tab.Screen name="Sign out" component={LogoutScreen} />
-          <Tab.Screen name="Practice" component={Practice} />
         </Tab.Navigator>
       </NavigationContainer>
     );
