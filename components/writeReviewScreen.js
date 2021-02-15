aa9a45c27080fce7145708d7d802eebb31a78bb3
TianneMarshall <tianne.marshall@stu.mmu.ac.uk>
2021-02-15 20:42:38 +0000

    initial commit of post review request screen

---
+++ components/writeReviewScreen.js
@@ -0,0 +1,192 @@
+import React, { Component } from 'react';
+import { Alert, Button, FlatList, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Toast from 'react-native-simple-toast';
+import { AirbnbRating } from 'react-native-ratings';
+import Stars from 'react-native-stars';
+
+class WriteReviewScreen extends Component {
+  constructor(props){
+    super(props)
+
+    this.state={
+      overall_rating: '',
+      review_body: '',
+      quality_rating: '',
+      price_rating: '',
+      clenliness_rating: '',
+    }
+
+  }
+
+  postReview = async () => {
+    const location_id = this.props.route.params.locId;
+    const token = await AsyncStorage.getItem('@session_token');
+
+    return fetch("http://10.0.2.2:3333/api/1.0.0/location/" + location_id + "/review",
+    {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Authorization': token
+        },
+        body: JSON.stringify({
+          overall_rating:  parseInt(this.state.overall_rating),
+          price_rating: parseInt(this.state.price_rating),
+          quality_rating: parseInt(this.state.quality_rating),
+          clenliness_rating: parseInt(this.state.clenliness_rating),
+          review_body:  this.state.review_body
+        })
+    })
+    .then((response) => {
+      if(response.status == 201) {
+        Toast.show("Posted review!");
+      }
+      else if(response.status == 400) {
+        throw "Error invalid review";
+      }
+      else if(response.status == 401) {
+        throw "Error cannot post review"
+      }
+      else if(response.status == 404) {
+        throw "Error cannot find location"
+      }
+      else {
+        throw "Error failed"
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+  }
+
+  profantiyCheck() {
+    const body = this.state.review_body.toLowerCase();
+    if(body.includes("cake")){
+      console.error("Error must not review cakes");
+    }
+    else if(body.includes("tea")){
+      console.error("Error must not review tea");
+    }
+    else if(body.includes("pastry") || body.includes("pastries")){
+      console.error("Error must not review pastries");
+    }
+    else{
+      this.postReview();
+    }
+
+  }
+
+  render() {
+    return(
+      <View style={styles.review}>
+
+      <ScrollView>
+          <View style={styles.rating}>
+            <TextInput
+              defaultValue="Quality"
+              editable={false}
+            />
+
+            <AirbnbRating
+              count={5}
+              defaultRating={0}
+              reviews=''
+              size={30}
+              onFinishRating={(quality_rating) => this.setState({quality_rating: quality_rating})}>
+            </AirbnbRating>
+          </View>
+
+
+
+          <View style={styles.rating}>
+            <TextInput
+              defaultValue="Price"
+              editable={false}
+            />
+
+            <AirbnbRating
+              count={5}
+              defaultRating={0}
+              reviews=''
+              size={30}
+              onFinishRating={(price_rating) => this.setState({price_rating: price_rating})}>
+            </AirbnbRating>
+          </View>
+
+
+
+          <View style={styles.rating}>
+            <TextInput
+              defaultValue="Cleanliness"
+              editable={false}
+            />
+
+            <AirbnbRating
+              count={5}
+              defaultRating={0}
+              reviews=''
+              size={30}
+              onFinishRating={(clenliness_rating) => this.setState({clenliness_rating: clenliness_rating})}>
+            </AirbnbRating>
+          </View>
+
+
+        <View style={styles.rating}>
+          <TextInput
+            defaultValue="Overall"
+            editable={false}
+          />
+
+          <AirbnbRating
+            count={5}
+            defaultRating={0}
+            reviews=''
+            size={30}
+            onFinishRating={(overall_rating) => this.setState({overall_rating: overall_rating})}>
+          </AirbnbRating>
+        </View>
+
+
+        <TextInput
+          style={styles.revBody}
+          placeholder="write review body"
+          onChangeText={(review_body) => this.setState({review_body: review_body})}
+          value={this.state.review_body}
+        />
+
+        <Button
+          title="Post Review"
+          onPress={() => this.profantiyCheck()}
+        />
+
+        </ScrollView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  review: {
+    flex: 1,
+    alignItems: 'center'
+  },
+
+  rating: {
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    textAlign: 'center'
+  },
+
+  revBody: {
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    height: 100,
+    width: 200,
+    borderColor: 'blue',
+    borderWidth: 3
+  }
+})
+
+export default WriteReviewScreen;
