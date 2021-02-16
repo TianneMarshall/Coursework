a52b19f254f2a7646a8db52a9a02511cbc19cb18
TianneMarshall <tiannemarshall@outlook.com>
2021-02-16 04:52:36 +0000

    Add config and build files due to build error

--- ios/Coursework2Tests/Coursework2Tests.m
+++ ios/Coursework2Tests/Coursework2Tests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface Coursework1Tests : XCTestCase
+@interface Coursework2Tests : XCTestCase
 
 @end
 
-@implementation Coursework1Tests
+@implementation Coursework2Tests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
