04d57d8788b0ef85126d20fd396fea86ad1ee27b
TianneMarshall <tiannemarshall@outlook.com>
2021-02-18 00:58:39 +0000

    Added react native camera to myReviews screen

--- components/myReviews.js
+++ components/myReviews.js
@@ -3,6 +3,7 @@ import { Alert, Button, FlatList, StyleSheet, Text, TextInput, View, TouchableOp
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Toast from 'react-native-simple-toast';
+import { RNCamera } from 'react-native-camera';
 
 import Review from './review';
 
@@ -54,15 +55,47 @@ class MyReviews extends Component {
     })
   }
 
-  addPhoto = async () => {
+
+  takePicture = async() => {
+    const token = await AsyncStorage.getItem('@session_token');
+    const location_id = this.state.location_id.toString();
+    const review_id = this.state.review_id.toString();
+
+    if(this.camera){
+      const options = {quality:0.5, base64:true}
+      const data = await this.camera.takePictureAsync(options);
+
+      console.log(data.uri, token);
+    }
+
     return fetch("http://10.0.2.2:3333/api/1.0.0/location/" + location_id + "/review/" + review_id + "/photo",
     {
       method: 'POST',
       headers: {
-        'Content-Type': 'image/png',
+        'Content-Type': 'image/jpeg',
         'X-Authorization': token
+      },
+      body: data
+    })
+    .then((response) => {
+      if(response.status == 200){
+        Toast.show("Photo Taken!");
       }
-      .then
+      else if(response.status == 400) {
+        throw "Error bad request";
+      }
+      else if(response.status == 401) {
+        throw "Error Unauthorised";
+      }
+      else if(response.status == 404) {
+        throw "Error not found";
+      }
+      else if(response.status == 500) {
+        throw "Error Failed";
+      }
+    })
+    .catch((error) => {
+      console.error(error);
     })
   }
 
@@ -71,8 +104,14 @@ class MyReviews extends Component {
     const review = this.props.reviewData;
 
     return(
-      <View>
-
+      <View style={styles.reviews}>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          captureAudio={false}
+        />
         <FlatList
           data={review}
           renderItem={({item}) =>
@@ -81,7 +120,8 @@ class MyReviews extends Component {
 
               <Button
                 title="Add Photo"
-                onPress={() => this.addPhoto()}
+                onPressIn={() => this.setState({review_id: item.review.review_id, location_id: item.location.location_id})}
+                onPress={() => this.takePicture()}
               />
 
               <TouchableOpacity
@@ -102,6 +142,9 @@ class MyReviews extends Component {
 }
 
 const styles = StyleSheet.create({
+  reviews: {
+    flex: 1
+  },
   delete: {
     alignItems: 'center'
   },
